de8601429c9019745157eca798291118b6c5ad9a
silviaalexasilva <55801093+silviaalexasilva@users.noreply.github.com>
2019-12-17 21:35:11 +0000

    Update MinAndMax.js

--- MinAndMax.js
+++ MinAndMax.js
@@ -1,8 +1,8 @@
 function findMinMax(arr) {
-    let min = arr[0].y, max = arr[0].y;
+    let min = arr[0], max = arr[0];
   
     for (let i = 1, len=arr.length; i < len; i++) {
-      let v = arr[i].y;
+      let v = arr[i];
       min = (v < min) ? v : min;
       max = (v > max) ? v : max;
     }
