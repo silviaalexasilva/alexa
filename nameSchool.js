05ab05161afeefdbc1a844d1d30d9e5a4035280c
silviaalexasilva <55801093+silviaalexasilva@users.noreply.github.com>
2019-12-17 21:35:48 +0000

    Update nameSchool.js

--- nameSchool.js
+++ nameSchool.js
@@ -1,16 +1,17 @@
-function nameSchool (num) {
-    for (i=1; i<=1000; i++) { 
-      if ( i % 3 === 0 && i % 5 === 0) { 
-        console.log("WildCode"); }
-      else if ( i % 3 === 0) {
-        console.log("Wild"); }
-       
-      else if ( i % 5 === 0) {
-          console.log("Code"); }
-      else {
-        console.log(i);
+
+       function nameSchool (num) {
+    
+        if ( num % 3 === 0 && num % 5 === 0) { 
+          console.log("WildCode"); }
+        else if ( num % 3 === 0) {
+          return ("Wild"); }
+         
+        else if ( num % 5 === 0) {
+            return ("Code"); }
+        else {
+          return (num);
+        }
       }
-    }
-    }
+  
   
   
