756cae07318ba184ec330d4d125a51129fbc7ae2
silviaalexasilva <55801093+silviaalexasilva@users.noreply.github.com>
2019-12-17 20:55:55 +0000

    Add files via upload

---
+++ nameSchool.js
@@ -0,0 +1,16 @@
+function nameSchool (num) {
+    for (i=1; i<=1000; i++) { 
+      if ( i % 3 === 0 && i % 5 === 0) { 
+        console.log("WildCode"); }
+      else if ( i % 3 === 0) {
+        console.log("Wild"); }
+       
+      else if ( i % 5 === 0) {
+          console.log("Code"); }
+      else {
+        console.log(i);
+      }
+    }
+    }
+  
+  
